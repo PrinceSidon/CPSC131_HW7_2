6ca3ce7ea86c48953e2e5f5fb0e02d18e57614f1
Siddharth Vasu <80875915+PrinceSidon@users.noreply.github.com>
2022-12-08 00:14:30 +0000

    Comments

--- main.cpp
+++ main.cpp
@@ -11,35 +11,41 @@ class Solution {
                                                           // or equal to bomb a's blast radius squared
     }
     
-    int bfs(int bid, int len, vector<vector<int>> adj)
+    int bfs(int bid, int len, vector<vector<int>> adj)    // Breadth-first search implementation
     {
         int val = 1;
-        vector<int> blasted(len, 0);
-        queue<int> q;
-        q.push(bid);
+        vector<int> blasted(len, 0);                      // The vector stores the bombs that have been
+                                                          // blasted
+      
+        queue<int> q;                                     // The queue stores the bombs that haven't been
+                                                          // blasted yet
+      
+        q.push(bid);      
         blasted[bid]=1;
-        int blastedTillNow = 1;
-        while(!q.empty())
+        int blastedTillNow = 1;                           // Stores the number of bombs that have exploded
+      
+        while(!q.empty())                                 // BFS ends when all adjacent bombs are found
         {
             int f = q.front();
             q.pop();
             for(auto k : adj[f])
             {
-                if(!blasted[k])
+                if(!blasted[k])                           // Makes sure this bomb hasn't already exploded
                 {
                     q.push(k);
                     blasted[k]=1;
                     blastedTillNow++;
-                    val = max(val, blastedTillNow);
+                    val = max(val, blastedTillNow);       // At least one bomb must explode
                     
                     
                 }
             }
         }
-        return val;
+        return val;                                       // Returns the number of blasted bombs
     }
         
-    int maximumDetonation(vector<vector<int>>& bombs) {
+    int maximumDetonation(vector<vector<int>>& bombs) {   // Returns the max number of bombs that
+                                                          // can be blasted at once
         
         vector<vector<int>> adj(bombs.size());            // The adjacency list of all the bombs
                                                           
@@ -60,14 +66,15 @@ class Solution {
         
         
         int ans = 0;
-        // perform bfs considering every bomb as source , find the max ans
-        for(int i=0; i<bombs.size(); i++)
+
+        for(int i=0; i<bombs.size(); i++)                  // Loops through the adjacency list
+                                                           // and calls BFS for each bomb
         {
             int x = bfs(i, bombs.size(), adj);
             ans = max(ans, x);
         }
         
-        return ans;
+        return ans;                                        // Returns the max number
         
       
     }
